ca832bdac7d7eeb9a77e1cebff4a5c4475140759
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-01-20 23:00:14 +0900

    DP lv4, 도둑질 오답 블로깅

--- src/programmers/고득점kit/dp/도둑질_해답.java
+++ src/programmers/고득점kit/dp/도둑질_해답.java
@@ -1,39 +1,38 @@
 package programmers.고득점kit.dp;
 
-// dp[i] = 도둑이 i 번째 집에 왔을 때 훔진 돈의 값
-// dp[i] = max(dp[i-2]+현재 m, dp[i-1]);
-// 집이 원형이기 때문에 first idx 와 last idx 둘 중 하나만 선택
+import java.util.Arrays;
+
+import static java.lang.Math.max;
+
 public class 도둑질_해답 {
 
     public int solution(int[] money) {
         int answer = 0;
+        int houseNum = money.length;
 
         // 집이 3개만 있을 경우는 한 곳만 훔칠 수 있음.
-        int len = money.length;
-        if (len == 3) {
-            for(int i = 0; i < 3; i++) answer = Math.max(answer, money[i]);
-        }
+        if (houseNum == 3) return Arrays.stream(money).max().getAsInt();
 
-        int[] dpForGetFirst = new int[len];
-        int[] dpForIgnoreFirst = new int[len];
+        int[] dpForGetFirst = new int[houseNum];
+        int[] dpForIgnoreFirst = new int[houseNum];
 
         // 1. 첫 집을 무조건 훔쳤을 때
         dpForGetFirst[0] = money[0];
-        dpForGetFirst[1] = Math.max(money[0],money[1]);
+        dpForGetFirst[1] = max(money[0], money[1]);
 
-        // 2. 첫 집을 훔치지 않고, 마지막 집을 훔쳤을 때
+        // 2. 첫 집을 훔치지 않았을 때
         dpForIgnoreFirst[0] = 0;
         dpForIgnoreFirst[1] = money[1];
 
-        for (int i = 2; i < len; i++) {
-            dpForIgnoreFirst[i] = Math.max(dpForIgnoreFirst[i-2] + money[i], dpForIgnoreFirst[i-1]);
-            answer = Math.max(answer, dpForIgnoreFirst[i]);
+        int i = 2;
+        for (; i < houseNum; i++) {
+            dpForIgnoreFirst[i] = max(dpForIgnoreFirst[i - 2] + money[i], dpForIgnoreFirst[i - 1]);
 
-            if(i == len-1) break; // 첫 집을 훔친 경우는 마지막 집을 훔치지 않기 때문에
-            dpForGetFirst[i] = Math.max(dpForGetFirst[i-2] + money[i], dpForGetFirst[i-1]);
-            answer = Math.max(answer, dpForGetFirst[i]);
+            // 첫 집을 훔친 경우는 마지막 집을 훔치지 않기 때문에 마지막 집에서 break;
+            if (i == houseNum - 1) break;
+            dpForGetFirst[i] = max(dpForGetFirst[i - 2] + money[i], dpForGetFirst[i - 1]);
         }
 
-        return answer;
+        return max(dpForIgnoreFirst[i], dpForGetFirst[i]);
     }
 }
