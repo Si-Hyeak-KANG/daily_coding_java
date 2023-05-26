89bcb0ac57634810e169f2539e970903283ba35e
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-05-26 16:21:13 +0900

    230526 [update] 시간복잡도 연산 공부

--- src/letsdoIt/random/BOJ_치킨치킨치킨_16439.java
+++ src/letsdoIt/random/BOJ_치킨치킨치킨_16439.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 // 1<=N(회원)<=30, 3<=M(치킨 종류의 수)<=30
 // 최대 세 가지 종류의 치킨만 주문
 // 시킨 치킨 세 마리 중 가장 선호도가 큰 값으로 적용
-// O(NM^3)
+// O(NM^3) -> N * M^3 => 최대 810,000 번 연산
 public class BOJ_치킨치킨치킨_16439 {
     // 내부에 M 만큼한 배열을 가질, 크기가 N인 배열을 만든다.
     // 재귀를 통해서 세 개의 열을 선택 - 생각해보니 굳이..?
@@ -21,6 +21,8 @@ public class BOJ_치킨치킨치킨_16439 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+
+        long startTime = System.currentTimeMillis();
 
         members = new int[N][M];
         for (int i = 0; i < N; i++) {
@@ -37,6 +39,9 @@ public class BOJ_치킨치킨치킨_16439 {
                 }
             }
         }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("start-end = " + (endTime-startTime));
 
         System.out.println(answer);
     }
